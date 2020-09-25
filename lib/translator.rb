04a683b2f136219ca48cb6945aefe88a0bbc1354
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-25 02:07:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   
   final_hash = load_library(file_path)
-  final_hash.map do |value|
+  final_hash.map do |key, value|
     binding.pry
     
   end
